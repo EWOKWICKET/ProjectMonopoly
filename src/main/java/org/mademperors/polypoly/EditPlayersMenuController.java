6eed99130213a0bd22e487887338220861eb43e4
Maksym <maksumsemen2006@gmail.com>
2024-06-06 21:43:18 +0300

    Added background image for menu

--- src/main/java/org/mademperors/polypoly/EditPlayersMenuController.java
+++ src/main/java/org/mademperors/polypoly/EditPlayersMenuController.java
@@ -2,19 +2,23 @@ package org.mademperors.polypoly;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class EditPlayersMenuController {
+public class EditPlayersMenuController  implements Initializable {
 
     @FXML
     private HBox player1Field;
@@ -157,4 +161,10 @@ public class EditPlayersMenuController {
         }
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        BorderPane borderPane= (BorderPane) startGame.getParent().getParent().getParent();
+        borderPane.getStyleClass().add("border-pane");
+      //  System.out.println(startGame.getParent().getParent().getParent().getClass());
+    }
 }
